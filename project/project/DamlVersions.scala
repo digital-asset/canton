079a3c236abb5ae126ac06e9663711203f7dd225
canton-machine <48923836+canton-machine@users.noreply.github.com>
2024-03-06 06:53:44 +0100

    Update 2024-03-05.23 (#141)
    Reference commit: 7d7547eaa6
    
    Co-authored-by: Canton <canton@digitalasset.com>

--- project/project/DamlVersions.scala
+++ project/project/DamlVersions.scala
@@ -7,7 +7,7 @@ object DamlVersions {
 
   /** The version of the daml compiler (and in most cases of the daml libraries as well).
     */
-  val version: String = "2.9.0-snapshot.20240225.12644.0.vbbb82ba8"
+  val version: String = "2.9.0-snapshot.20240228.12647.1.v85afe011"
 
   /** Custom Daml artifacts override version.
     */
